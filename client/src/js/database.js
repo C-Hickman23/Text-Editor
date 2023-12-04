74a78a3b8315fd7e4888c2cd2a7ae00fb40b7b1c
Cardon Hickman <cardonhickman@gmail.com>
2023-12-03 22:29:01 -0700

    added webpack and database stuff

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,38 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method to add content to the database
+export const putDb = async (content) => {
+  const textD = await openDb('jate', 1);
+  const tx = textDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const request = store.put({ id: 1, value: content })
+
+  request.onsuccess = (event) => {
+    console.log('Data added to the database successfully');
+  };
+
+  request.onerror = (event) => {
+    console.error('Error adding data to the database', event.target.error);
+  };
+
+  await tx.done;
+};
+
+// Method to get all content from the database
+export const getDb = async () => {
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);  
+  
+  console.log('All content from the database:', request);
+
+  await tx.done;
+
+  return request;
+};
 
 initdb();
